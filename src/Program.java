e254287116fb7abe1bce178fd5d74b77508f32cf
andrei-kislushchanka <akislushchanka@bk.ru>
2022-05-19 17:35:10 +0300

    my 1st change

--- src/Program.java
+++ src/Program.java
@@ -2,6 +2,7 @@ public class Program
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Мой первый репозиторий");
+		System.out.println("Мой первый репозиторий!!!");
+		System.out.println("Мое первое изменение");
 	}
 }
